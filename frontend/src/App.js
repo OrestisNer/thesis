9fc8ef8b9d17ef8a5e44f07eafd5d265dc0057d6
OrestisNer <orestis@ep-mail.gr>
2021-02-25 19:50:39 +0200

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button
+          variant="contained"
+          style={{
+            fontSize: 40
+          }}
+        >
+          Hello World
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div >
+  );
+}
+
+export default App;
